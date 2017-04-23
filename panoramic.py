be5f03c4e466a2eb0991981357ff8147dfd73541
ronrest <ronny.coding@gmail.com>
2017-04-23 22:53:35 +1000

    DOCS: Add panoramic.py with only a description
    - No code added yet

---
+++ panoramic.py
@@ -0,0 +1,9 @@
+"""====================================================
+                    DESCRIPTION
+
+Functions for creating panoramic "front views" from a
+point cloud.
+=======================================================
+"""
+__author__ = 'Ronny Restrepo'
+

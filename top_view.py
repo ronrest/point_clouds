4dd65133865862fa68931e3316de4299bb7b1a65
ronrest <ronny.coding@gmail.com>
2017-04-24 21:29:22 +1000

    FEAT: Add `point_cloud_2_birdseye()` function

--- top_view.py
+++ top_view.py
@@ -20,3 +20,55 @@ def scale_to_255(a, min, max, dtype=np.uint8):
     """
     return (((a - min) / float(max - min)) * 255).astype(dtype)
 
+def point_cloud_2_birdseye(points,
+                           res=0.1,
+                           side_range=(-10., 10.),  # left-most to right-most
+                           fwd_range = (-10., 10.), # back-most to forward-most
+                           height_range=(-2., 2.),  # bottom-most to upper-most
+                           ):
+    # EXTRACT THE POINTS FOR EACH AXIS
+    x_points = points[:, 0]
+    y_points = points[:, 1]
+    z_points = points[:, 2]
+
+    # FILTER - To return only indices of points within desired cube
+    # Three filters for: Front-to-back, side-to-side, and height ranges
+    # Note left side is positive y axis in LIDAR coordinates
+    f_filt = np.logical_and((x_points > fwd_range[0]), (x_points < fwd_range[1]))
+    s_filt = np.logical_and((y_points > -side_range[1]), (y_points < -side_range[0]))
+    filter = np.logical_and(f_filt, s_filt)
+    indices = np.argwhere(filter).flatten()
+
+    # KEEPERS
+    x_points = x_points[indices]
+    y_points = y_points[indices]
+    z_points = z_points[indices]
+
+    # CONVERT TO PIXEL POSITION VALUES - Based on resolution
+    x_img = (-y_points / res).astype(np.int32)  # x axis is -y in LIDAR
+    y_img = (-x_points / res).astype(np.int32)  # y axis is -x in LIDAR
+
+    # SHIFT PIXELS TO HAVE MINIMUM BE (0,0)
+    # floor & ceil used to prevent anything being rounded to below 0 after shift
+    x_img -= int(np.floor(side_range[0] / res))
+    y_img += int(np.ceil(fwd_range[1] / res))
+
+    # CLIP HEIGHT VALUES - to between min and max heights
+    pixel_values = np.clip(a=z_points,
+                           a_min=height_range[0],
+                           a_max=height_range[1])
+
+    # RESCALE THE HEIGHT VALUES - to be between the range 0-255
+    pixel_values = scale_to_255(pixel_values,
+                                min=height_range[0],
+                                max=height_range[1])
+
+    # INITIALIZE EMPTY ARRAY - of the dimensions we want
+    x_max = 1 + int((side_range[1] - side_range[0]) / res)
+    y_max = 1 + int((fwd_range[1] - fwd_range[0]) / res)
+    im = np.zeros([y_max, x_max], dtype=np.uint8)
+
+    # FILL PIXEL VALUES IN IMAGE ARRAY
+    im[y_img, x_img] = pixel_values
+
+    return im

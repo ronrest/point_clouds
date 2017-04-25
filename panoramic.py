e40693ee1b6fb59f58b2776f8f07ec445da4a6bc
ronrest <ronny.coding@gmail.com>
2017-04-25 22:00:57 +1000

    FEAT: Add `scale_to_255()` to panoramic.py

--- panoramic.py
+++ panoramic.py
@@ -7,3 +7,8 @@ point cloud.
 """
 __author__ = 'Ronny Restrepo'
 
+import numpy as np
+
+def scale_to_255(a, min, max, dtype=np.uint8):
+    return (((a - min) / float(max - min)) * 255).astype(dtype)
+

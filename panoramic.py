6ae8a97f45e634ea7b4233d278a4b1cec3c78e60
ronrest <ronny.coding@gmail.com>
2017-04-25 22:01:37 +1000

    DOCS: Add docstring to `scale_to_255()` in panoramic.py

--- panoramic.py
+++ panoramic.py
@@ -9,6 +9,12 @@ __author__ = 'Ronny Restrepo'
 
 import numpy as np
 
+# ==============================================================================
+#                                                                   SCALE_TO_255
+# ==============================================================================
 def scale_to_255(a, min, max, dtype=np.uint8):
+    """ Scales an array of values from specified min, max range to 0-255
+        Optionally specify the data type of the output (default is uint8)
+    """
     return (((a - min) / float(max - min)) * 255).astype(dtype)
 

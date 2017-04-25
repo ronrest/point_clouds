75ba925d8951d2ccdd3337eda7bc71a7a7205488
ronrest <ronny.coding@gmail.com>
2017-04-25 22:02:38 +1000

    FEAT: Add `point_cloud_to_panorama()` to panoramic.py

--- panoramic.py
+++ panoramic.py
@@ -18,3 +18,55 @@ def scale_to_255(a, min, max, dtype=np.uint8):
     """
     return (((a - min) / float(max - min)) * 255).astype(dtype)
 
+
+def point_cloud_to_panorama(points,
+                            v_res=0.42,
+                            h_res = 0.35,
+                            v_fov = (-24.9, 2.0),
+                            d_range = (0,100),
+                            y_fudge=3
+                            ):
+    # Projecting to 2D
+    x_points = points[:, 0]
+    y_points = points[:, 1]
+    z_points = points[:, 2]
+    r_points = points[:, 3]
+    d_points = np.sqrt(x_points ** 2 + y_points ** 2)  # map distance relative to origin
+    #d_points = np.sqrt(x_points**2 + y_points**2 + z_points**2) # abs distance
+
+    # We use map distance, because otherwise it would not project onto a cylinder,
+    # instead, it would map onto a segment of slice of a sphere.
+
+    # RESOLUTION AND FIELD OF VIEW SETTINGS
+    v_fov_total = -v_fov[0] + v_fov[1]
+
+    # CONVERT TO RADIANS
+    v_res_rad = v_res * (np.pi / 180)
+    h_res_rad = h_res * (np.pi / 180)
+
+    # MAPPING TO CYLINDER
+    x_img = np.arctan2(y_points, x_points) / h_res_rad
+    y_img = -(np.arctan2(z_points, d_points) / v_res_rad)
+
+    # THEORETICAL MAX HEIGHT FOR IMAGE
+    d_plane = (v_fov_total/v_res) / (v_fov_total* (np.pi / 180))
+    h_below = d_plane * np.tan(-v_fov[0]* (np.pi / 180))
+    h_above = d_plane * np.tan(v_fov[1] * (np.pi / 180))
+    y_max = int(np.ceil(h_below+h_above + y_fudge))
+
+    # SHIFT COORDINATES TO MAKE 0,0 THE MINIMUM
+    x_min = -360.0 / h_res / 2
+    x_img = np.trunc(-x_img - x_min).astype(np.int32)
+    x_max = int(np.ceil(360.0 / h_res))
+
+    y_min = -((v_fov[1] / v_res) + y_fudge)
+    y_img = np.trunc(y_img - y_min).astype(np.int32)
+
+    # CLIP DISTANCES
+    d_points = np.clip(d_points, a_min=d_range[0], a_max=d_range[1])
+
+    # CONVERT TO IMAGE ARRAY
+    img = np.zeros([y_max + 1, x_max + 1], dtype=np.uint8)
+    img[y_img, x_img] = scale_to_255(d_points, min=d_range[0], max=d_range[1])
+
+    return img

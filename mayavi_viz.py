10d31685cce3ffe084fd4d2d52cc398861475093
ronrest <ronny.coding@gmail.com>
2017-04-26 22:12:42 +1000

    FEAT: Add `viz_mayavi()` to `mayavi_viz.py`

--- mayavi_viz.py
+++ mayavi_viz.py
@@ -6,4 +6,29 @@ library
 """
 __author__ = 'Ronny Restrep'
 
+import numpy as np
+import mayavi.mlab
+
+def viz_mayavi(points, vals="distance"):
+    x = points[:, 0]  # x position of point
+    y = points[:, 1]  # y position of point
+    z = points[:, 2]  # z position of point
+    # r = points[:, 3]  # reflectance value of point
+    d = np.sqrt(x ** 2 + y ** 2)  # Map Distance from sensor
+
+    if vals == "height":
+        col = z
+    else:
+        col = d
+
+    fig = mayavi.mlab.figure(bgcolor=(0, 0, 0), size=(640, 360))
+    mayavi.mlab.points3d(x, y, z,
+                         col,          # Values used for Color
+                         mode="point",
+                         colormap='spectral', # 'bone', 'copper', 'gnuplot'
+                         # color=(0, 1, 0),   # Used a fixed (r,g,b) instead
+                         figure=fig,
+                         )
+    mayavi.mlab.show()
+    
     

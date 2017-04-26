847293481804aa45b30109776efd9f52ff09e2fc
ronrest <ronny.coding@gmail.com>
2017-04-26 22:20:20 +1000

    DOCS: Add docstring to `viz_mayavi()`

--- mayavi_viz.py
+++ mayavi_viz.py
@@ -9,7 +9,25 @@ __author__ = 'Ronny Restrep'
 import numpy as np
 import mayavi.mlab
 
+
+# ==============================================================================
+#                                                                     VIZ_MAYAVI
+# ==============================================================================
 def viz_mayavi(points, vals="distance"):
+    """ Interactively visualize the point cloud data using mayavi
+    
+    Args:
+        points: (numpy array)
+            array of shape at least nx3 containing the point cloud data
+            n rows representing each point.
+            3 or more values for each point, with the first three
+            representing the x,y,z coordinates
+        vals: (str) (default="distance")
+            What values to use for color coding.
+            "distance" = the calculated distance of each point from the origin
+                         when looked at from the top
+            "height" = the z values of the point cloud data
+    """
     x = points[:, 0]  # x position of point
     y = points[:, 1]  # y position of point
     z = points[:, 2]  # z position of point

d97549089b7e19f56b9e388c44fc63e767c0a31b
ronrest <ronny.coding@gmail.com>
2017-04-26 22:09:39 +1000

    DOCS: Add `mayavi_viz.py` with only a description

---
+++ mayavi_viz.py
@@ -0,0 +1,9 @@
+"""====================================================
+                    DESCRIPTION
+Visualisations of point cloud data using the mayavi
+library
+=======================================================
+"""
+__author__ = 'Ronny Restrep'
+
+    
